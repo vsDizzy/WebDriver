0287d8279ee2805dfe8380dec4aa79b121918cb3
vsDizzy <pkopytin@gmail.com>
2015-07-20 14:08:37 +0300

    ` unknown error Could not load IE WebDriver interface

---
+++ WebDriver/Program.cs
@@ -0,0 +1,16 @@
+﻿using IEWebDriverLib;
+
+namespace WebDriver
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			const string cmd =
+				@"{""name"": ""newSession"", ""parameters"": {""desiredCapabilities"": {}, ""requiredCapabilities"":{}}, ""sessionId"": null}";
+			var wd = new IEWebDriverManager();
+			string response;
+			wd.ExecuteCommand(cmd, out response);
+		}
+	}
+}
